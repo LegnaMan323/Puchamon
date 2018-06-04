538b0228428b2be75b6387d7b945dad9444a4d9d
david <idv16a.arodriguez@uartesdigitales.edu.mx>
2018-06-04 01:32:35 -0500

    iv

---
+++ Puchamon/PoekmonV1/PoekmonV1/IPony.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoekmonV1
+{
+    interface IPony
+    {
+        string PName { get; set; }
+        string PTipo { get; set; }
+
+        public int pLife { get; set; }
+        public int pAtk { get; set; }
+        public int pDef { get; set; }
+
+        void Attack();
+        void Heal();
+        void Defend();
+        void run();
+    }
+}
